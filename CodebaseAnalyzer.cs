d7877fb2175fbeac5eb490aa5e367d6bbe80c406
pvanoostenbrugge <pvanoostenbrugge@gmail.com>
2023-03-21 00:12:46 +0100

    add classinfo

---
+++ CodebaseAnalyzer.cs
@@ -0,0 +1,167 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis.MSBuild;
+
+namespace CodebaseAnalysisLib
+{
+    public class CodebaseAnalyzer
+    {
+        public static async Task<CodebaseInfo> GetCodebaseInfo(string solutionPath, string userInput)
+        {
+            userInput ??= string.Empty;
+            var workspace = MSBuildWorkspace.Create();
+            var solution = await workspace.OpenSolutionAsync(solutionPath);
+            var codebaseInfo = new CodebaseInfo();
+
+            var projects = solution.Projects.ToList();
+
+            var tasks = projects.Select(async project =>
+            {
+                var updatedProject = project.WithAllSourceFiles();
+                var compilation = await updatedProject.GetCompilationAsync();
+                var excludes = new List<string> { @"\obj\", @"\platforms\" };
+                var excludedRegex = new Regex(string.Join("|", excludes.Select(Regex.Escape)), RegexOptions.IgnoreCase);
+
+                foreach (var syntaxTree in compilation.SyntaxTrees.Where(tree => !excludedRegex.IsMatch(tree.FilePath)))
+                {
+                    var semanticModel = compilation.GetSemanticModel(syntaxTree);
+                    //var diagnostics = await GetDiagnosticsAsync(compilation);
+                    //codebaseInfo.DiagnosticsByProject[project.AssemblyName] = diagnostics.ToList();
+
+                    var root = await syntaxTree.GetRootAsync();
+                    var classDeclarations = root.DescendantNodes().OfType<ClassDeclarationSyntax>();
+                    foreach (var classDeclaration in classDeclarations)
+                    {
+                        var className = classDeclaration.Identifier.ValueText;
+                        ExtractClassMembers(classDeclaration, semanticModel, codebaseInfo, className);
+                        codebaseInfo.ProjectNames.Add(project.AssemblyName);
+                        if (!codebaseInfo.ClassNames.ContainsKey(project.AssemblyName))
+                        {
+                            codebaseInfo.ClassNames.Add(project.AssemblyName, new List<string>());
+                        }
+                        codebaseInfo.ClassNames[project.AssemblyName].Add(className);
+                        // Find base types and implemented interfaces
+                        var baseTypesAndInterfaces = new List<string>();
+                        foreach (var baseType in classDeclaration.BaseList?.Types ?? Enumerable.Empty<BaseTypeSyntax>())
+                        {
+                            var typeSymbol = semanticModel.GetTypeInfo(baseType.Type).Type;
+                            if (typeSymbol != null)
+                            {
+                                baseTypesAndInterfaces.Add(typeSymbol.Name);
+                            }
+                        }
+                        if (baseTypesAndInterfaces.Count > 0)
+                        {
+                            codebaseInfo.ClassRelationships[className] = baseTypesAndInterfaces;
+                        }
+
+                        if (userInput.Contains(className))
+                        {
+                            if (userInput.Contains(className))
+                            {
+                                var classSymbol = semanticModel.GetDeclaredSymbol(classDeclaration);
+                                var classSyntax = classSymbol.DeclaringSyntaxReferences.First().GetSyntax();
+                                codebaseInfo.FullCodeOfClass[className] = classSyntax.GetText().ToString();
+                            }
+                        }
+
+                        var classMethods = classDeclaration.DescendantNodes().OfType<MethodDeclarationSyntax>().ToList();
+                        var methodNames = new List<string>();
+                        foreach (var methodDeclaration in classMethods)
+                        {
+                            var methodName = methodDeclaration.Identifier.ValueText;
+                            methodNames.Add(methodName);
+                            if (userInput.Contains(methodName))
+                            {
+                                var methodSymbol = semanticModel.GetDeclaredSymbol(methodDeclaration);
+                                var methodSyntax = methodSymbol.DeclaringSyntaxReferences.First().GetSyntax();
+                                codebaseInfo.FullCodeOfMethods[methodName] = methodSyntax.GetText().ToString();
+                            }
+                        }
+                        if (!codebaseInfo.ClassMethods.ContainsKey(className))
+                        {
+                            codebaseInfo.ClassMethods.Add(className, methodNames);
+                        }
+                    }
+                }
+            }).ToArray();
+            await Task.WhenAll(tasks);
+
+            return codebaseInfo;
+        }
+
+        private static void ExtractClassMembers(ClassDeclarationSyntax classDeclaration, SemanticModel semanticModel, CodebaseInfo codebaseInfo, string className)
+        {
+            var properties = classDeclaration.DescendantNodes().OfType<PropertyDeclarationSyntax>().ToList();
+            var fields = classDeclaration.DescendantNodes().OfType<FieldDeclarationSyntax>().ToList();
+            var events = classDeclaration.DescendantNodes().OfType<EventDeclarationSyntax>().ToList();
+            var attributes = classDeclaration.AttributeLists.SelectMany(al => al.Attributes).ToList();
+
+            var propertyNames = properties.Select(p => p.Identifier.ValueText).ToList();
+            var fieldNames = fields.SelectMany(f => f.Declaration.Variables.Select(v => v.Identifier.ValueText)).ToList();
+            var eventNames = events.Select(e => e.Identifier.ValueText).ToList();
+            var attributeNames = attributes.Select(a => semanticModel.GetTypeInfo(a).Type.Name).ToList();
+
+            lock (codebaseInfo)
+            {
+                codebaseInfo.ClassProperties[className] = propertyNames;
+                codebaseInfo.ClassFields[className] = fieldNames;
+                codebaseInfo.ClassEvents[className] = eventNames;
+                codebaseInfo.ClassAttributes[className] = attributeNames;
+            }
+        }
+
+        private static async Task<IEnumerable<Diagnostic>> GetDiagnosticsAsync(Compilation compilation)
+        {
+            var analyzers = GetAllAnalyzers();
+            var compilationWithAnalyzers = compilation.WithAnalyzers(analyzers);
+            var diagnostics = await compilationWithAnalyzers.GetAllDiagnosticsAsync();
+            return diagnostics;
+        }
+
+        private static ImmutableArray<DiagnosticAnalyzer> GetAllAnalyzers()
+        {
+            var assemblyLocation = typeof(CodebaseAnalyzer).Assembly.Location;
+            var assemblyDirectory = Path.GetDirectoryName(assemblyLocation);
+            var analyzerAssemblyPath = Path.Combine(assemblyDirectory, "Analyzers", "Microsoft.CodeAnalysis.NetAnalyzers.dll");
+
+            var analyzerAssembly = Assembly.LoadFrom(analyzerAssemblyPath);
+            var analyzerTypes = analyzerAssembly.GetTypes().Where(type => type.IsSubclassOf(typeof(DiagnosticAnalyzer)) && !type.IsAbstract);
+            var analyzers = analyzerTypes.Select(type => (DiagnosticAnalyzer)Activator.CreateInstance(type)).ToImmutableArray();
+            return analyzers;
+        }
+
+    }
+
+
+
+    static class ProjectExtensions
+    {
+        public static Project AddDocuments(this Project project, IEnumerable<string> files)
+        {
+            foreach (string file in files)
+            {
+                project = project.AddDocument(file, File.ReadAllText(file)).Project;
+            }
+            return project;
+        }
+
+        private static IEnumerable<string> GetAllSourceFiles(string directoryPath) => Directory.GetFiles(directoryPath, "*.cs", SearchOption.AllDirectories);
+
+
+        public static Project WithAllSourceFiles(this Project project)
+        {
+            string projectDirectory = Directory.GetParent(project.FilePath).FullName;
+            var files = GetAllSourceFiles(projectDirectory);
+            var newProject = project.AddDocuments(files);
+            return newProject;
+        }
+    }
+}
+
--- CodebaseInfo.cs
+++ CodebaseInfo.cs
@@ -2,25 +2,40 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.MSBuild;
 
 namespace CodebaseAnalysisLib
 {
     public class CodebaseInfo
     {
+        public CodebaseInfo()
+        {
+            DiagnosticsByProject = new Dictionary<string, List<Diagnostic>>();
+            ProjectNames = new List<string>();
+            ClassNames = new Dictionary<string, List<string>>();
+            ClassMethods = new Dictionary<string, List<string>>();
+            ClassProperties = new Dictionary<string, List<string>>();
+            ClassFields = new Dictionary<string, List<string>>();
+            ClassEvents = new Dictionary<string, List<string>>();
+            ClassAttributes = new Dictionary<string, List<string>>();
+            ClassRelationships = new Dictionary<string, List<string>>();
+            FullCodeOfClass = new Dictionary<string, string>();
+            FullCodeOfMethods = new Dictionary<string, string>();
+        }
+
         public List<string> ProjectNames { get; set; } = new List<string>();
+        public Dictionary<string, List<Diagnostic>> DiagnosticsByProject { get; set; } = new Dictionary<string, List<Diagnostic>>();
         public Dictionary<string, List<string>> ClassNames { get; set; } = new Dictionary<string, List<string>>();
         public Dictionary<string, List<string>> ClassMethods { get; set; } = new Dictionary<string, List<string>>();
+        public Dictionary<string, List<string>> ClassProperties { get; set; } = new Dictionary<string, List<string>>();
+        public Dictionary<string, List<string>> ClassFields { get; set; } = new Dictionary<string, List<string>>();
+        public Dictionary<string, List<string>> ClassEvents { get; set; } = new Dictionary<string, List<string>>();
+        public Dictionary<string, List<string>> ClassAttributes { get; set; } = new Dictionary<string, List<string>>();
+        public Dictionary<string, List<string>> ClassRelationships { get; set; } = new Dictionary<string, List<string>>();
         public Dictionary<string, string> FullCodeOfClass { get; set; } = new Dictionary<string, string>();
         public Dictionary<string, string> FullCodeOfMethods { get; set; } = new Dictionary<string, string>();
-        public Dictionary<string, List<string>> ClassRelationships { get; set; } = new Dictionary<string, List<string>>();
 
 
         public override string ToString()
@@ -29,6 +44,15 @@ namespace CodebaseAnalysisLib
             foreach (var projectName in ProjectNames.Distinct())
             {
                 sb.AppendLine($"Project: {projectName}");
+
+                if (DiagnosticsByProject.ContainsKey(projectName) && DiagnosticsByProject[projectName].Any())
+                {
+                    sb.AppendLine("  Diagnostics:");
+                    foreach (var diagnostic in DiagnosticsByProject[projectName])
+                    {
+                        sb.AppendLine($"    - {diagnostic.Id}: {diagnostic.GetMessage()}");
+                    }
+                }
 
                 foreach (var className in ClassNames[projectName])
                 {
@@ -45,9 +69,44 @@ namespace CodebaseAnalysisLib
                         sb.Append("    Inherits/Implements: ");
                         sb.AppendLine(string.Join(", ", ClassRelationships[className]));
                     }
-                    else { sb.AppendLine(); }
 
-                    
+                    if (ClassProperties.ContainsKey(className) && ClassProperties[className].Any())
+                    {
+                        sb.AppendLine("    Properties:");
+                        foreach (var propertyName in ClassProperties[className])
+                        {
+                            sb.AppendLine($"      - {propertyName}");
+                        }
+                    }
+
+                    if (ClassFields.ContainsKey(className) && ClassFields[className].Any())
+                    {
+                        sb.AppendLine("    Fields:");
+                        foreach (var fieldName in ClassFields[className])
+                        {
+                            sb.AppendLine($"      - {fieldName}");
+                        }
+                    }
+
+                    if (ClassEvents.ContainsKey(className) && ClassEvents[className].Any())
+                    {
+                        sb.AppendLine("    Events:");
+                        foreach (var eventName in ClassEvents[className])
+                        {
+                            sb.AppendLine($"      - {eventName}");
+                        }
+                    }
+
+                    if (ClassAttributes.ContainsKey(className) && ClassAttributes[className].Any())
+                    {
+                        sb.AppendLine("    Attributes:");
+                        foreach (var attributeName in ClassAttributes[className])
+                        {
+                            sb.AppendLine($"      - {attributeName}");
+                        }
+                    }
+
+
                     if (ClassMethods[className].Any())
                     {
                         sb.AppendLine($"    Methods: ");
@@ -62,123 +121,10 @@ namespace CodebaseAnalysisLib
                         }
                     }
                     sb.AppendLine();
-
-
-
                 }
             }
-
             return sb.ToString();
         }
-    }
-
-    public class CodebaseAnalyzer
-    {
-        public async Task<CodebaseInfo> GetCodebaseInfo(string solutionPath, string userInput)
-        {
-            var workspace = MSBuildWorkspace.Create();
-            var solution = await workspace.OpenSolutionAsync(solutionPath);
-            var codebaseInfo = new CodebaseInfo();
-
-            foreach (var project in solution.Projects)
-            {
-                var updatedProject = project.WithAllSourceFiles();
-                var compilation = await updatedProject.GetCompilationAsync();
-                var excludes = new List<string> { @"\obj\", @"\platforms\" };
-                var excludedRegex = new Regex(string.Join("|", excludes.Select(Regex.Escape)), RegexOptions.IgnoreCase);
-
-
-                foreach (var syntaxTree in compilation.SyntaxTrees.Where(tree => !excludedRegex.IsMatch(tree.FilePath)))
-                {
-                    var semanticModel = compilation.GetSemanticModel(syntaxTree);
-                    var root = await syntaxTree.GetRootAsync();
-
-                    var classDeclarations = root.DescendantNodes().OfType<ClassDeclarationSyntax>();
-                    foreach (var classDeclaration in classDeclarations)
-                    {
-                        codebaseInfo.ProjectNames.Add(project.AssemblyName);
-
-                        var className = classDeclaration.Identifier.ValueText;
-                        if (!codebaseInfo.ClassNames.ContainsKey(project.AssemblyName))
-                        {
-                            codebaseInfo.ClassNames.Add(project.AssemblyName, new List<string>());
-                        }
-                        codebaseInfo.ClassNames[project.AssemblyName].Add(className);
-                        // Find base types and implemented interfaces
-                        var baseTypesAndInterfaces = new List<string>();
-                        foreach (var baseType in classDeclaration.BaseList?.Types ?? Enumerable.Empty<BaseTypeSyntax>())
-                        {
-                            var typeSymbol = semanticModel.GetTypeInfo(baseType.Type).Type;
-                            if (typeSymbol != null)
-                            {
-                                baseTypesAndInterfaces.Add(typeSymbol.Name);
-                            }
-                        }
-                        if (baseTypesAndInterfaces.Count > 0)
-                        {
-                            codebaseInfo.ClassRelationships[className] = baseTypesAndInterfaces;
-                        }
-
-                        if (userInput.Contains(className))
-                        {
-                            if (userInput.Contains(className))
-                            {
-                                var classSymbol = semanticModel.GetDeclaredSymbol(classDeclaration);
-                                var classSyntax = classSymbol.DeclaringSyntaxReferences.First().GetSyntax();
-                                codebaseInfo.FullCodeOfClass[className] = classSyntax.GetText().ToString();
-                            }
-                        }
-
-                        var classMethods = classDeclaration.DescendantNodes().OfType<MethodDeclarationSyntax>().ToList();
-                        var methodNames = new List<string>();
-                        foreach (var methodDeclaration in classMethods)
-                        {
-                            var methodName = methodDeclaration.Identifier.ValueText;
-                            methodNames.Add(methodName);
-                            if (userInput.Contains(methodName))
-                            {
-                                var methodSymbol = semanticModel.GetDeclaredSymbol(methodDeclaration);
-                                var methodSyntax = methodSymbol.DeclaringSyntaxReferences.First().GetSyntax();
-                                codebaseInfo.FullCodeOfMethods[methodName] = methodSyntax.GetText().ToString();
-                            }
-                        }
-                        if (!codebaseInfo.ClassMethods.ContainsKey(className))
-                            codebaseInfo.ClassMethods.Add(className, methodNames);
-                    }
-                }
-            }
-
-            return codebaseInfo;
-        }
-
-    }
-}
-
-static class ProjectExtensions
-{
-    public static Project AddDocuments(this Project project, IEnumerable<string> files)
-    {
-        foreach (string file in files)
-        {
-            project = project.AddDocument(file, File.ReadAllText(file)).Project;
-        }
-        return project;
-    }
-
-    private static IEnumerable<string> GetAllSourceFiles(string directoryPath)
-    {
-        var res = Directory.GetFiles(directoryPath, "*.cs", SearchOption.AllDirectories);
-
-        return res;
-    }
-
-
-    public static Project WithAllSourceFiles(this Project project)
-    {
-        string projectDirectory = Directory.GetParent(project.FilePath).FullName;
-        var files = GetAllSourceFiles(projectDirectory);
-        var newProject = project.AddDocuments(files);
-        return newProject;
     }
 }
 
